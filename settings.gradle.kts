492e8a6cae3206d2c086b67f30837d57115e1f85
chan <chan.mobiledev@gmail.com>
2026-04-07 13:44:05 +0530

    include

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,9 +19,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
 rootProject.name = "Zetta_Package"
-include(":app")
 include(":zettalibrary")
