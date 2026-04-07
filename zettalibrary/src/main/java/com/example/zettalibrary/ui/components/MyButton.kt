2c68b878eb28278024b9dcd9a14630901d17d60e
chan <chan.mobiledev@gmail.com>
2026-04-07 13:40:27 +0530

    Initial commit

---
+++ zettalibrary/src/main/java/com/example/zettalibrary/ui/components/MyButton.kt
@@ -0,0 +1,26 @@
+package com.example.zettalibrary.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MyButton(){
+
+
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()) {
+
+
+        TextButton(onClick = {}) {
+
+            Text("Test")
+        }
+    }
+}
